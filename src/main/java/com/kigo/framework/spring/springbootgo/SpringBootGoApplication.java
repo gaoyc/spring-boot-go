20f3c598dd962e641ee03f1712693c0d76ed581b
kigo <keepgo@qq.com>
2018-08-16 18:38:15 +0800

    SPRING INITIALIZR original version

---
+++ src/main/java/com/kigo/framework/spring/springbootgo/SpringBootGoApplication.java
@@ -0,0 +1,12 @@
+package com.kigo.framework.spring.springbootgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kigo/framework/spring/springbootgo/SpringBootGoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kigo.framework.spring.springbootgo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootGoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
